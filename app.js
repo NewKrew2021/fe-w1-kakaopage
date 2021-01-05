6655b8a5176661c4e32f33a8e4d963b01bad829f
석찬희 <roddy@roddy-pc.local>
2021-01-05 11:57:15 +0900

    # feature: 신규 기능 및 유지보수 건     # bug: 버그 수정     # etc: 기타 변경 feat : public, views 구조 생성 pulbic, views 디렉토리에 css, html 파일 등 추가

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.set('views', __dirname + '/views');
+app.use(express.static('public'));
+
+app.get('',(req,res)=>{
+    res.sendFile(__dirname + '/views/'+ 'index.html');
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
