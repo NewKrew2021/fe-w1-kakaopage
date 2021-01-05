722a3db9b5966ffd59349e9cc10e106a05529474
nina.oh <nina.oh@kakaocorp.com>
2021-01-05 18:56:07 +0900

    Add active underline for main tabbar

--- app.js
+++ app.js
@@ -10,25 +10,39 @@ app.use(express.static('public'))
 
 // routes
 app.get('/', (req, res) => {
-    res.render('dummyPage', {})
+    res.render('dummyPage', {
+        page: req.path.split('/')[1]
+    })
 })
 app.get('/home', (req, res) => {
-    res.render('dummyPage', {})
+    res.render('dummyPage', {
+        page: req.path.split('/')[1]
+    })
 })
 app.get('/webtoon', (req, res) => {
-    res.render('index', {})
+    res.render('index', {
+        page: req.path.split('/')[1]
+    })
 })
 app.get('/webnovel', (req, res) => {
-    res.render('dummyPage', {})
+    res.render('dummyPage', {
+        page: req.path.split('/')[1]
+    })
 })
 app.get('/movie', (req, res) => {
-    res.render('dummyPage', {})
+    res.render('dummyPage', {
+        page: req.path.split('/')[1]
+    })
 })
 app.get('/broadcast', (req, res) => {
-    res.render('dummyPage', {})
+    res.render('dummyPage', {
+        page: req.path.split('/')[1]
+    })
 })
 app.get('/book', (req, res) => {
-    res.render('dummyPage', {})
+    res.render('dummyPage', {
+        page: req.path.split('/')[1]
+    })
 })
 
 
