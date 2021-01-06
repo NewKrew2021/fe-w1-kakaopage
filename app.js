12508f7978b5532a0ebf1e0396a82cc2807813f2
석찬희 <roddy@roddy-pc.local>
2021-01-06 11:00:05 +0900

    refactor : ejs 엔진 사용하여 반복되는 코드 정리 ejs 사용, 코드 정리, css 항목 일부 수정

--- app.js
+++ app.js
@@ -1,13 +1,57 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = 3001;
 
-app.set('views', __dirname + '/views');
-app.use(express.static('public'));
+/* data */
+const day_slide_numtxt = ['1/3', '2/3', '3/3'];
+const day_slide_title = ['핫차!핫차! 차나무생', '사실, 그들은 오직 그녀만을 기다리고 있습니다', '불사무적'];
+const day_slide_explain = ['노는 게 제일 좋아~ 어른이 모여라!', '그녀는 어디로 갔단 말입니까?', '다시 태어난 불사존의 활약!'];
+const day_slide_img = ["/images/daytoon1.png", "/images/daytoon2.png", "/images/daytoon3.png"];
+const slide_numtxt = ['1/4', '2/4', '3/4', '4/4'];
+const slide_img = ["/images/toon3.png","/images/toon1.png","/images/toon4.png","/images/toon2.png"]
+const slide_title = ['마족의 계약', '빅 라이프', '까마귀 우는 밤', '트리거'];
+const slide_explain = ['마계공주, 인간세계에서 무료봉사 중?!', '안 팔리는 무명 작가의 인생 대반전!', '우리 다시 만날 수 있어요?', '내 안의 화끈한 방아쇠를 당겨라!'];
+const slide_subtxt_img = ['/images/badge-up.svg','/images/badge-up.svg','/images/badge-up.svg','/images/badge-up.svg'];
+const slide_subtxt_dir = ['웹툰','웹툰','웹툰','웹툰'];
+const slide_subtxt_person = ['42.7만명','43.1만명','34.1만명','33.5만명'];
+const home_banner_1_txt = ['오늘 UP', '오늘 신작', '오리지널'];
+const home_banner_1_attr = ['first','second','third'];
+const home_banner_1_cnt = ['161','','1,250'];
 
-app.get('/',(req,res)=>{
-    res.sendFile(__dirname + '/views/'+ 'index.html');
-});
+var day_slide = [
+  { numtxt: day_slide_numtxt[0], img: day_slide_img[0], title: day_slide_title[0], exp: day_slide_explain[0] },
+  { numtxt: day_slide_numtxt[1], img: day_slide_img[1], title: day_slide_title[1], exp: day_slide_explain[1] },
+  { numtxt: day_slide_numtxt[2], img: day_slide_img[2], title: day_slide_title[2], exp: day_slide_explain[2] },
+];
+
+var home_slide = [
+  { numtxt: slide_numtxt[0], img: slide_img[0], title: slide_title[0], exp: slide_explain[0],
+    subtxt_img: slide_subtxt_img[0], subtxt_dir: slide_subtxt_dir[0], subtxt_person: slide_subtxt_person[0]
+  },
+  { numtxt: slide_numtxt[1], img: slide_img[1], title: slide_title[1], exp: slide_explain[1],
+    subtxt_img: slide_subtxt_img[1], subtxt_dir: slide_subtxt_dir[1], subtxt_person: slide_subtxt_person[1]
+  },
+  { numtxt: slide_numtxt[2], img: slide_img[2], title: slide_title[2], exp: slide_explain[2],
+    subtxt_img: slide_subtxt_img[2], subtxt_dir: slide_subtxt_dir[2], subtxt_person: slide_subtxt_person[2]
+  },
+  { numtxt: slide_numtxt[3], img: slide_img[3], title: slide_title[3], exp: slide_explain[3],
+    subtxt_img: slide_subtxt_img[3], subtxt_dir: slide_subtxt_dir[3], subtxt_person: slide_subtxt_person[3]
+  },
+];
+
+var homeBanner = [
+  {text: home_banner_1_txt[0], attr: home_banner_1_attr[0], cnt: home_banner_1_cnt[0]},
+  {text: home_banner_1_txt[1], attr: home_banner_1_attr[1], cnt: home_banner_1_cnt[1]},
+  {text: home_banner_1_txt[2], attr: home_banner_1_attr[2], cnt: home_banner_1_cnt[2]}
+]
+
+app.set('view engine', 'ejs');
+app.use(express.static(__dirname + '/public'));
+app.set('views', './views');
+
+app.get('/', function(req, res){
+  res.render('index', { home_slide, homeBanner, day_slide });
+})
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -17,42 +17,35 @@ const content_fantasy = document.getElementById("fantasy");
 const content_action = document.getElementById("action");
 const content_blgl = document.getElementById("blgl");
 
-/* image List */
-const slide_img_1 = '/images/toon3.png';
-const slide_img_2 = '/images/toon1.png';
-const slide_img_3 = '/images/toon4.png';
-const slide_img_4 = '/images/toon2.png';
+const topbannerImg = ['/images/banner1.png', '/images/banner2.png', '/images/banner3.png'];
 
-const day_slide_img_1 = '/images/daytoon1.png';
-const day_slide_img_2 = '/images/daytoon2.png';
-const day_slide_img_3 = '/images/daytoon3.png';
+/* banner에 이미지 추가 */
+document.getElementById("banner_prev").style.backgroundImage = "url('/images/before.svg')";
+document.getElementById("banner_next").style.backgroundImage = "url('/images/next.svg')";
+document.getElementById("banner_img").setAttribute('src','/images/banner1.png');
 
-/* Title List */
-const slide_numtxt = ['1/4', '2/4', '3/4', '4/4'];
-const slide_title = ['마족의 계약', '빅 라이프', '까마귀 우는 밤', '트리거'];
-const slide_explain = ['마계공주, 인간세계에서 무료봉사 중?!', '안 팔리는 무명 작가의 인생 대반전!', '우리 다시 만날 수 있어요?', '내 안의 화끈한 방아쇠를 당겨라!'];
-const slide_subtxt = ["<img src='/images/badge-up.svg'> 웹툰 <img src='/images/line_top_banner.png'> <img src='/images/ico-bigthum-person.svg'> 42.7만명",
-"<img src='/images/badge-up.svg'> 웹툰 <img src='/images/line_top_banner.png'> <img src='/images/ico-bigthum-person.svg'> 43.1만명",
-"<img src='/images/badge-up.svg'> 웹툰 <img src='/images/line_top_banner.png'> <img src='/images/ico-bigthum-person.svg'> 34.1만명",
-"<img src='/images/badge-up.svg'> 웹툰 <img src='/images/line_top_banner.png'> <img src='/images/ico-bigthum-person.svg'> 33.5만명"];
-
-const day_slide_numtxt = ['1/3','2/3','3/3'];
-const day_slide_title = ['핫차!핫차! 차나무생','사실, 그들은 오직 그녀만을 기다리고 있습니다','불사무적'];
-const day_slide_explain = ['노는 게 제일 좋아~ 어른이 모여라!', '그녀는 어디로 갔단 말입니까?', '다시 태어난 불사존의 활약!'];
-
-/* slide 객체 */
-const home_slide = [
-    { numtxt: slide_numtxt[0], img: slide_img_1, title: slide_title[0], exp: slide_explain[0], subtxt : slide_subtxt[0]},
-    { numtxt: slide_numtxt[1], img: slide_img_2, title: slide_title[1], exp: slide_explain[1], subtxt : slide_subtxt[1] },
-    { numtxt: slide_numtxt[2], img: slide_img_3, title: slide_title[2], exp: slide_explain[2], subtxt : slide_subtxt[2] },
-    { numtxt: slide_numtxt[3], img: slide_img_4, title: slide_title[3], exp: slide_explain[3], subtxt : slide_subtxt[3] }
-]
-
-const day_slide = [
-    { numtxt: day_slide_numtxt[0], img: day_slide_img_1, title: day_slide_title[0], exp: day_slide_explain[0] },
-    { numtxt: day_slide_numtxt[1], img: day_slide_img_2, title: day_slide_title[1], exp: day_slide_explain[1] },
-    { numtxt: day_slide_numtxt[2], img: day_slide_img_3, title: day_slide_title[2], exp: day_slide_explain[2] },
-]
+/* 배너 이미지 변경하기 */
+var count = 0;
+document.getElementById("banner_prev").addEventListener("click", ()=>{
+    if(count > 0){
+        count--;
+        document.getElementById("banner_img").setAttribute('src',topbannerImg[count]);
+    }
+    else if (count <= 0){
+        count = 2;
+        document.getElementById("banner_img").setAttribute('src',topbannerImg[count]);
+    }
+});
+document.getElementById("banner_next").addEventListener("click", ()=>{
+    if(count < 2){
+        count++;
+        document.getElementById("banner_img").setAttribute('src',topbannerImg[count]);
+    }
+    else if (count >= 2){
+        count = 0;
+        document.getElementById("banner_img").setAttribute('src',topbannerImg[count]);
+    }
+});
 
 /* 웹툰 메뉴 이외에는 dummy 보여주기 */
 function showDummyArea() {
@@ -82,21 +75,8 @@ for (var i = 0; i < topNavList.length; i++) {
     });
 }
 
-/* [웹툰 > 홈]의 경우 이미지, 텍스트 초기 로딩 */
-function showInitialHome() {
-    for (var i = 0; i < 4; i++) {
-        document.getElementById("slide_num" + String(i + 1)).innerHTML = home_slide[i].numtxt;
-        document.getElementById("slide_img" + String(i + 1)).src = home_slide[i].img;
-        document.getElementById("slide_txt" + String(i + 1)).innerHTML = home_slide[i].title;
-        document.getElementById("slide_exp" + String(i + 1)).innerHTML = home_slide[i].exp;
-        document.getElementById("slide_subtxt" + String(i + 1)).innerHTML = home_slide[i].subtxt;
-    }
-}
-showInitialHome();
-
 var slideIndex = 0;
 var timer;
-showSlides("mySlides");
 
 /* [웹툰] 슬라이드 애니메이션 함수 */
 function showSlides(slidename) {
@@ -127,27 +107,22 @@ for (var i = 0; i < contentNavList.length; i++) {
             document.getElementById(this.classList[1]).className.replace(" invisible", " visible");
 
         /* 클릭 후 이미지, 텍스트 로딩 */
-        var show_content = document.getElementById(this.classList[1]);
+        var show_content = this.classList[1];
+        console.log(show_content);
 
         /* 홈 tab */
-        if (show_content.id === "home") {   
+        if (show_content == "home") {   
             slideIndex = 0;
             clearTimeout(timer);
             showSlides('mySlides'); 
         }
 
         /* 요일연재 tab */
-        else if (show_content.id === "day") {    
-            for (var i = 0; i < 3; i++) {
-                document.getElementById("day_slide_num" + String(i + 1)).innerHTML = day_slide[i].numtxt;
-                document.getElementById("day_slide_img" + String(i + 1)).src = day_slide[i].img;
-                document.getElementById("day_slide_txt" + String(i + 1)).innerHTML = day_slide[i].title;
-                document.getElementById("day_slide_exp" + String(i + 1)).innerHTML = day_slide[i].exp;
-            }
+        else if (show_content == "day") { 
             slideIndex = 0;
             clearTimeout(timer);
             showSlides('daySlides'); 
         }
     })
 }
-
+document.getElementById("content_home").click();
