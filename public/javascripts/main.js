fb1a9049bbbb3d2f1c4fca1e93c8ddc4abc5d830
puba.yu <puba.yu@kakaocorp.com>
2021-01-06 22:11:19 +0900

    feat : webtoon genre nav를 제작하는 코드 작성
    - webtoon genre nav를 제작하는 코드 작성, HTML과 CSS 코드가 아닌 DOM API를 이용하여 제작
    - genre nav는 자주 사용되므로, 범용성 있는 코드로 작성

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,4 +1,15 @@
 const HEADER_NAV_LIST = ['홈', '웹툰', '웹소설', '영화', '방송', '책'];
+const WEBTOON_NAV_LIST = [
+  '홈',
+  '요일연재',
+  '웹툰',
+  '소년',
+  '드라마',
+  '로맨스',
+  '로판',
+  '액션무협',
+  'BL/GL',
+];
 const HEADER_NAV_ITEM_CLASS_NAME = 'header-nav__item';
 const HEADER_NAV_CLICKED_ITEM_CLASS_NAME = 'header-nav__item--clicked';
 const LIST_TAG = 'li';
@@ -34,8 +45,29 @@ const renderHeaderNav = () => {
   );
 };
 
+const createGenreNav = (navItemList, navClassName) => {
+  const genreNav = $(`.${navClassName}`);
+  const genreNavList = createNewElement('ul', 'genre-nav__list', '');
+
+  navItemList.forEach(navItem => {
+    const navItemElement = createNewElement(
+      LIST_TAG,
+      'genre-nav__item',
+      navItem
+    );
+    genreNavList.appendChild(navItemElement);
+  });
+  genreNav.appendChild(genreNavList);
+  return genreNav;
+};
+
+const renderWebtoonPage = () => {
+  createGenreNav(WEBTOON_NAV_LIST, 'webtoon-genre-nav');
+};
+
 const render = () => {
   renderHeaderNav();
+  renderWebtoonPage();
 };
 
 window.addEventListener('DOMContentLoaded', () => render());
